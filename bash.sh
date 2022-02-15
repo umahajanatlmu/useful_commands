e44415b60df5632277c854ca0b7c27cfe7ead8a5
Ujjwal Mahajan <67412222+umahajanatlmu@users.noreply.github.com>
2022-02-15 14:20:16 +0100

    format

--- bash.sh
+++ bash.sh
@@ -3,9 +3,9 @@
 diff -r /Volumes/G_DRIVE/Ujjwal_lab/multiplexing/Didem/Analysis/3_aligned_stacks/ /Volumes/G_DRIVE/Ujjwal_lab/multiplexing/Didem/Analysis/4_artifacts_output/ | grep /Volumes/G_DRIVE/Ujjwal_lab/multiplexing/Didem/Analysis/3_aligned_stacks/ | awk '{print $4}' > defin.txt
 sed "/and/d" defin.txt > defin1.txt; rm defin.txt
 
-while read FILENAME; 
+while IFS= read -r file
 do
-find $PWD/Analysis/3_aligned_stacks -name "*${FILENAME}*" -exec cp '{}' $PWD/Analysis/5_artifacts_removal \; 
+    cp -i -- $PWD/Analysis/3_aligned_stacks/"$file" $PWD/Analysis/5_artifacts_removal/
 done < defin1.txt
 
 
