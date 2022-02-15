4d0db43c9ee97aa5c873d9bcf98a4ec084a12a55
Ujjwal Mahajan <67412222+umahajanatlmu@users.noreply.github.com>
2022-02-15 11:33:31 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,11 @@
 ## find file differences from two folder and copy file to third folder
 
-rsync -rvcm --compare-dest={parent_folder} {daughter_folder} {diff_folder}
+diff -r /Volumes/G_DRIVE/Ujjwal_lab/multiplexing/Didem/Analysis/3_aligned_stacks/ /Volumes/G_DRIVE/Ujjwal_lab/multiplexing/Didem/Analysis/4_artifacts_output/ | grep /Volumes/G_DRIVE/Ujjwal_lab/multiplexing/Didem/Analysis/3_aligned_stacks/ | awk '{print $4}' > defin.txt
+sed "/and/d" defin.txt > defin1.txt; rm defin.txt
+
+while read FILENAME; 
+do
+find $PWD/Analysis/3_aligned_stacks -name "*${FILENAME}*" -exec cp '{}' $PWD/Analysis/5_artifacts_removal \; 
+done < defin1.txt
+
+
